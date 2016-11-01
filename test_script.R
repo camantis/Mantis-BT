915f3aeab5bbfc27a2b0474295e48d865684d00d
camantis <camjones@ucdavis.edu>
2016-11-01 14:00:54 -0700

    Linear model simulation
    added linear model and created a graph

--- test_script.R
+++ test_script.R
@@ -2,3 +2,18 @@
 x<-rnorm(n=1000, mean=0,sd=3)
 x
 hist(x, nclass=20)
+
+###Linear regression
+x<-runif(1000,0,100)
+x
+hist(x)
+
+###Set parameters
+m<-3
+b<- -2
+eps<-rnorm(1000,mean=0,sd=100)
+y<-m*x+b+eps
+plot(y)
+plot(x,y)
+ #Reduce size of the points
+plot(x,y,cex=.5,col="blue")
