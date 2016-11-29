cdc31cbb2c77ffebc983a0697f3e20d3aacecf70
Cameron <camjones@ucdavis.edu>
2016-11-29 12:21:38 -0800

    adding select input

--- app.r
+++ app.r
@@ -17,43 +17,23 @@ dbListTables(con)##returns a vector object
 mantis<-dbGetQuery(con,"SELECT * From mantis;")
 View(mantis)
 mantids<-as.data.frame.matrix(mantis)
-rm(list = ls())
-library(lme4)
-library(bbmle)
-library(rptR)
-library(MASS)
-library(boot)
-library(beepr)
-library(lsmeans)
-library(piecewiseSEM)
-library(MuMIn)
+mantids$id <- as.factor(mantids$id)
+mantids$trial <- as.factor(mantids$trial)
+mantids$res <- as.factor(seq(1,length(mantids$id), 1))
+mantids$loss.p <- (mantids$start_mass - mantids$max_mass)/mantids$max_mass
+mantids$loss.p.c <- (mantids$loss.p - mean(mantids$loss.p, na.rm=TRUE))/(sd(mantids$loss.p, na.rm=TRUE))
 
-library(RPostgreSQL)
-drv<-dbDriver("PostgreSQL")
-con<-dbConnect(drv, dbname="lab1",host="localhost",port=5432,user="postgres",password="postgres")
-dbListTables(con)##returns a vector object
-mantis<-dbGetQuery(con,"SELECT * From mantis;")
-View(mantis)
-mantids<-as.data.frame.matrix(mantis)
-LTM<-c(0.4,0.502)
-TTLC<-c(0.489,0.192)
-TTS<-c(0.28,0.369)
-AAP<-c(0.697,0.657)
-AAS<-c(0.779,0.660)
-EAT<-c(0.192,0.468)
 
 ui <- fluidPage(
-  titlePanel("Comparing behavioral mean and variation across species"),
-  sidebarLayout(
-    sidebarPanel(
-      uiOutput("Behavior")
-    ),
-  mainPanel ("Display results",
-             plotOutput("bar"))
+  selectInput(inputId = "choose",
+              label="Choose Behavior",
+              choices=c("Latency to move", "Time to leave circle", 
+                        "Time to reach shelter", "Latency to approach novel prey", 
+                        "Time to strike novel prey", "Number of prey items eaten")),
+  plotOutput("behavior")
   )
-)
-
 
 server <- function(input, output) {}
+
 
 shinyApp(ui = ui, server = server)
