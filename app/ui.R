1664be350d2bd10d6f119ded88e4ec4b5d90c033
Cameron <camjones@ucdavis.edu>
2016-11-29 14:56:23 -0800

    Attempt at adding db connection

--- app/ui.R
+++ app/ui.R
@@ -1,13 +1,21 @@
 
 ui <- fluidPage(
   #Add theme to fluidpage 
-  selectInput(inputId = "choose",
+  sidebarLayout(
+    sidebarPanel(
+      textInput("postgresDBname", "Enter Database Name"),
+      textInput("postgresUser", "Enter username"),
+      textInput("postgresPort", "Enter Port number"),
+      textInput("postgresHost", "Enter host name"),
+      passwordInput ("postgresPwd","Enter password"),
+      selectInput(inputId = "choose",
               label="Choose Behavior",
               choices=c("Latency to move", "Time to leave circle", 
                         "Time to reach shelter", "Latency to approach novel prey", 
                         "Time to strike novel prey", "Number of prey items eaten")),
   plotOutput("behavior")
-  
+)
+)
 )
 
 
