e083963f456d8751ac42b95e8400dbeada0135ae
ljpollack <ljpollack@ucdavis.edu>
2016-12-03 17:37:03 -0800

    Add tabs to User Interface, include's Lea's UI
    Organized app into 3 tabs (connecting to database, Cameron's graphs, and Lea's graphs)

--- app/ui.R
+++ app/ui.R
@@ -1,20 +1,49 @@
 
 ui <- fluidPage(
   #Add theme to fluidpage 
-  sidebarLayout(
-    sidebarPanel(
-      textInput("postgresDBname", "Enter Database Name"),
-      textInput("postgresUser", "Enter username"),
-      textInput("postgresPort", "Enter Port number"),
-      textInput("postgresHost", "Enter host name"),
-      passwordInput ("postgresPwd","Enter password"),
-      selectInput(inputId = "choose",
+  ##Set up tabs on App
+ tabsetPanel(
+    #tab 1 connect to database
+    tabPanel("Connnect", "Connect to Database"),  
+     sidebarLayout(
+        sidebarPanel(
+          textInput("postgresDBname", "Enter Database Name"),
+          textInput("postgresUser", "Enter username"),
+          textInput("postgresPort", "Enter Port number"),
+          textInput("postgresHost", "Enter host name"),
+          passwordInput ("postgresPwd","Enter password"),
+          actionButton("postgresConnect", label = "Connect"),
+          br(),
+          br(),
+        textOutput("DoneConnect")
+    
+    #Tab for histograms comparing populations     
+    tabPanel("Copmaring Populations", "However you want to describe it"),
+      sidebarLayout(
+        sidebarPanel(
+          selectInput(inputId = "choose",
               label="Choose Behavior",
               choices=behavList)),
       mainPanel(
         textOutput("text1"),
         plotOutput("behavior")
+        
+   #Tab for scatter plots comparing behaviors   
+   tabPanel("Searching for Syndromes", "Check two behaviors to look for syndromes"),
+   sidebarLayout(
+      sidebarPanel(
+        br(),  
+        checkboxGroupInput("behaviors", "Behaviors:", choices = behavList),
+        br(),
+        br()
+      ),
+      mainPanel(
+        hr(),
+        ##show plot
+        plotOutput(OutputId = "synscatter")
       )
+  
+ 
 )
 )
 
