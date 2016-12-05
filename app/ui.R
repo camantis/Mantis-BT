327c448d7d560dc64c9d01229c1b6fe9e0318a75
Cameron <camjones@ucdavis.edu>
2016-12-04 23:32:38 -0800

    Adding some text and images

--- app/ui.R
+++ app/ui.R
@@ -21,8 +21,38 @@
       )
     )
     
-    
-    
+    tabPanel("welcome","Welcome to the CA Mantis Shiny App"),
+    sidebarLayout(
+      sidebarPanel(),
+      mainPanel(
+        img(src="cover.jpg"),
+        p("Welcome to the CA Mantis shiny app. With this application,
+          you will be able to visualize statistical results from a recent
+          comparing the behavior of two species of praying mantids"),
+        p("The CA Mantis study was designed to determine how the behavioral phenotype
+          structure differs between native and invasive species of praying mantids present
+          in northern California,"),
+        em("Stagmomantis limbata and Mantis religiosa"),
+        p(span(em("M. religiosa")), "is recently established and evidence suggests
+          it is displacing", span(em("S. limbata")), "from the local habitat."),
+        p("In this app, you will be able to look at the compare the results of
+the behavioral assays across species to look at both mean and variation of behaviors as 
+          well as correlations between behaviors."),
+        img(src="s_l.jpg"),
+        img(src="hopper.jpg"),
+        p("Number of prey items eaten refers to the voracity of individuals, 
+          which were allowed to capture as many prey items as possible in a
+          15 minute assay."),
+        p("'Latency to move', 'time to leave circle', and 'time to reach shelter'
+          refer to assays in which mantids were placed in an open, preceived 'risky'
+          arena, and were monitored to record the time at which they made their first
+movement, exited the area in which they were introduced, and reached shelter."),
+        p("Finally, individuals were exposed to a novel prey in the form of a fiddler crab.
+          Latency to begin approaching the crab and time it took to ellicit a predatory
+          strike was recorded.")
+      )
+    )
+
     #TEMP
     tabPanel("Searching for Syndromes", "Check two behaviors to look for syndromes"),
     sidebarLayout(
@@ -33,6 +63,7 @@
         br()
       ),
       mainPanel(
+        img(src="mantis.jpg")
         hr(),
         plotOutput(OutputID="synscatter",label="syn") 
       )
@@ -45,6 +76,7 @@
               label="Choose Behavior",
               choices=behavList)),
       mainPanel(
+        img(src="male.jpg"),
         textOutput("text1"),
         plotOutput("behavior"),
         hr(),
