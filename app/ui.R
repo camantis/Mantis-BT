43e9a067320f8fb2ac416ddcc64106f78860702b
Cameron <camjones@ucdavis.edu>
2016-12-04 17:53:05 -0800

    Added new UI to fix

--- app/ui.R
+++ app/ui.R
@@ -1,7 +1,41 @@
 
 ui <- fluidPage(
   #Add theme to fluidpage 
-  sidebarLayout(
+  #Tem
+  tabsetPanel(
+    tabPanel("Connect","Connect to Database"),
+    sidebarLayout(
+      sidebarPanel(
+        textInput("postgresDBname", "Enter Database Name"),
+        textInput("postgresUser","Enter username"),
+        textInput("postgresPort","Enter Port number"),
+        textInput("postgresHost","Enter host name"),
+        passwordInput("postgresPwd","Enter password"),
+        actionButton("postgresConnect",label="Connect"),
+        br(),
+        br(),
+        textOutput("DoneConnect")
+      )
+    )
+    
+    
+    
+    #TEMP
+    tabPanel("Searching for Syndromes", "Check two behaviors to look for syndromes"),
+    sidebarLayout(
+      sidebarPanel(
+        br(),
+        checkboxGroupInput("behaviors","Behaviors:",choices=behavList),
+        br(),
+        br()
+      ),
+      mainPanel(
+        hr(),
+        plotOutput(OutputID="synscatter",label="syn") 
+      )
+    )
+  tabPanel("Behavioral differences","Compare mean and variation of behavioral traits"),
+    sidebarLayout(
     sidebarPanel(
       textInput("postgresDBname", "Enter Database Name"),
       textInput("postgresUser", "Enter username"),
@@ -17,5 +51,6 @@ ui <- fluidPage(
         hr(),
         plotOutput("variation")
       )
+)
 )
 )
